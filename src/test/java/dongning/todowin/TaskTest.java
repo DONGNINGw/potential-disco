0b7f41cf1de27d9ada7ab4b0f9cee84a4d0bc420
Dongning Wang <wangdn.victor.0629@gmail.com>
2024-01-23 13:06:19 +0800

    待办事项测试模块和待办事项数据库测试模块

---
+++ src/test/java/dongning/todowin/TaskTest.java
@@ -0,0 +1,52 @@
+package dongning.todowin;
+
+import dongning.todowin.todo.Task;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskTest {
+    // 测试 Task 类的构造函数和 getter 方法
+    @Test
+    void  testTaskConstructorAndGetters(){
+        // 准备测试数据
+        Date testDate = new Date(System.currentTimeMillis());
+        // 创建一个 Task 对象
+        Task task = new Task(1, "Test", "Description", testDate, true,false);
+
+        // 使用断言测试 Task 对象的属性值是否正确
+        assertEquals(1, task.getId());
+        assertEquals("Test", task.getTitle());
+        assertEquals("Description", task.getDescription());
+        assertEquals(testDate, task.getDue_date());
+        assertTrue(task.getReminder());
+        assertFalse(task.getCompleted());
+    }
+
+    // 测试 Task 类的 setter 方法
+    @Test
+    void testSetters(){
+        //创建一个空的 Task 对象
+        Task task = new Task();
+        Date testDate = new Date(System.currentTimeMillis());
+
+        //设置 Task 对象的属性值
+        task.setId(1);
+        task.setTitle("Test");
+        task.setDescription("Description");
+        task.setDue_date(testDate);
+        task.setReminder(true);
+        task.setCompleted(false);
+
+        // 使用断言测试 Task 对象的属性值是否正确
+        assertEquals(1, task.getId());
+        assertEquals("Test", task.getTitle());
+        assertEquals("Description", task.getDescription());
+        assertEquals(testDate, task.getDue_date());
+        assertTrue(task.getReminder());
+        assertFalse(task.getCompleted());
+
+    }
+}
